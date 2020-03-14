c184e7df2f9ffff3f124fcfe5c6da889ab99be38
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-14 23:43:41 +0530

    chat application functional

--- src/orange/settings.py
+++ src/orange/settings.py
@@ -78,6 +78,16 @@ TEMPLATES = [
 ASGI_APPLICATION = 'orange.routing.application'
 WSGI_APPLICATION = 'orange.wsgi.application'
 
+ASGI_APPLICATION = 'orange.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 
 # Database
