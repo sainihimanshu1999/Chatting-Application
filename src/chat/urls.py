b24b10d4ea62f2eb6dd21005d6a5ce56652985b5
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-03-14 14:06:46 +0530

    third commit

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -1,11 +1,8 @@
-
 from django.urls import path
-from .views import index
 
-
-app_name = 'chat'
+from . import views
 
 urlpatterns = [
-    
-    path('', index , name ='index'),
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
